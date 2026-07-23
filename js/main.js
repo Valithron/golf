21324c66b7f31bdc45e60c5f12f6a8c33ea552b3
Valithron <125237424+Valithron@users.noreply.github.com>
2026-07-23 10:45:15 -0600

    Convert round bumper into playable sand patch

--- js/main.js
+++ js/main.js
@@ -18,6 +18,33 @@ function gameLoop(now){
 }
 
 buildCourse();
+
+const sandPatch=decorativeBumpers.find(item=>item.kind==="stone");
+if(sandPatch){
+  sandPatch.kind="sand";
+  sandPatch.points=[
+    {x:sandPatch.x-31,y:sandPatch.y-7},
+    {x:sandPatch.x-22,y:sandPatch.y-24},
+    {x:sandPatch.x-2,y:sandPatch.y-29},
+    {x:sandPatch.x+24,y:sandPatch.y-20},
+    {x:sandPatch.x+33,y:sandPatch.y-3},
+    {x:sandPatch.x+21,y:sandPatch.y+20},
+    {x:sandPatch.x-5,y:sandPatch.y+27},
+    {x:sandPatch.x-28,y:sandPatch.y+15}
+  ];
+  sandPatch.body.isSensor=true;
+  sandPatch.body.label="sand";
+  sandPatch.body.restitution=0;
+  Body.scale(sandPatch.body,1.25,.9);
+
+  Events.on(engine,"beforeUpdate",()=>{
+    if(state.sinking||!Vertices.contains(sandPatch.body.vertices,ball.position))return;
+    Sleeping.set(ball,false);
+    Body.setVelocity(ball,Vector.mult(ball.velocity,.978));
+    Body.setAngularVelocity(ball,ball.angularVelocity*.88);
+  });
+}
+
 resizeCanvas();
 updateHud();
 Sleeping.set(ball,true);
